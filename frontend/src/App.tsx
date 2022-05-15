531bf395e95251e69678f32bd7b2a483b56d12b1
seyoung8239 <seyoung8239@gmail.com>
2022-05-15 15:42:36 +0900

    Feat: set up frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Landing from './components/Landing';
+import SignIn from './components/SignIn';
+import Finder from './components/Finder';
+import SignUp from './components/SignUp';
+import Guest from './components/Guest';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return (<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/guest" element={<Guest />} />
+      <Route path="/finder" element={<Finder />} />
+    </Routes>
+  </BrowserRouter>)
 }
 
 export default App;
