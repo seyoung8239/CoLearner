0b6115f0dc70a0e2d14f2878d856fabe80ff3c8b
dnn12 <81808767+dnn12@users.noreply.github.com>
2022-05-31 21:05:06 +0900

    styles 랜딩페이지 css 수정

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
+import { useCallback } from 'react';
 import {useNavigate} from 'react-router-dom';
+import "./landing.css";
 
 type PageCategory = "signin" | "signup" | "guest";
 
@@ -23,11 +24,15 @@ const Landing = () => {
 
   return (
     <>
-      <button onClick={()=> handleClick("signin")}>로그인하기</button>
-      <button onClick={()=> handleClick("signup")}>회원가입 하기</button>
-      <button onClick={()=> handleClick("guest")}>비회원으로 사용하기</button>
+    <h1 className = "logo">Colearner</h1>
+    <div className = "btns">
+      <button className = "btn"  onClick={()=> handleClick("signin")}>로그인하기</button>
+      <button  className = "btn" onClick={()=> handleClick("signup")}>회원가입 하기</button>
+      <button className = "btn"  onClick={()=> handleClick("guest")}>비회원으로 사용하기</button>
+      </div>
     </>
   )
 }
+
 
 export default Landing;
