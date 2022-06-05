bff3980ca2455c468d2e508dae4b7757060bec3c
seyoung8239 <seyoung8239@gmail.com>
2022-06-05 15:12:07 +0900

    Feat: add guest page structure

--- frontend/src/pages/Guest/index.tsx
+++ frontend/src/pages/Guest/index.tsx
@@ -1,26 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { Document, Page, pdfjs } from 'react-pdf';
 
-import "./dropzone.css";
+import { BasicAPIResponseType } from '../../types';
+import { apiOrigin, requestGet } from '../../utils/api';
 
-const fileTypes = ["PDF"];
+import GuestUrlView from './GuestUrlView'
 
-function DragDrop() {
-  const [file, setFile] = useState(null);
-  const handleChange = (file : any) => {
-    setFile(file);
-  };
+pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.worker.js';
 
-  return (
-    <div className="dropzone">
-      {/* <FileUploader
-        label = "업로드할 파일을 Drag & Drop 해주세요"
-        multiple={true}
-        handleChange={handleChange}
-        name="file"
-        types={fileTypes}
-      /> */}
-    </div>
-  );
+const Guest = () => {
+  const [file, setFile] = useState<File>();
+  const [curPage, setCurPage] = useState<number>(1);
+  const [endPage, setEndPage] = useState<number>(100);
+  const [base64File, setBase64File] = useState<string>();
+  const [isLoadingFile, setIsLoadingFile] = useState<boolean>(true);
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleChangePage = (offset: number) => {
+    const newPage = curPage + offset;
+    if (newPage > 0 && newPage <= endPage) {
+      setCurPage(newPage);
+    } else
+      alert('해당 페이지로는 이동할 수 없습니다.');
+  }
+
+  const onDocumentLoadSuccess = (endPage: any) => {
+    console.log('endPage', endPage);
+    setEndPage(endPage._pdfInfo.numPages);
+  }
+
+  const handleChangeFile = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    let newFile = event.target.files![0]
+    setFile(newFile);
+  }, []);
+
+  const handleUploadFile = useCallback(() => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file! as Blob);
+    setBase64File(reader.result as string);
+    console.log(reader.result)
+    setIsLoadingFile(false);
+  }, []);
+
+  return (<>
+    {isLoadingFile ?
+      <div>
+        파일을 올려주세요
+      </div> :
+      <Document file={`data:application/pdf;base64,${base64File}`} onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error}>
+        <Page pageNumber={curPage} />
+      </Document>
+    }
+
+    <GuestUrlView curPage={curPage} />
+    <p>{curPage} / {endPage}</p>
+    <button name='before' onClick={() => handleChangePage(-1)}>이전</button>
+    <button name='before' onClick={() => handleChangePage(+1)}>다음</button>
+
+    <input type="file" onChange={handleChangeFile} />
+    <button onClick={handleUploadFile}>파일 업로드 하기</button>
+  </>)
 }
 
-export default DragDrop;
+export default Guest;
