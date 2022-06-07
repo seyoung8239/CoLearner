d519f1a0fb6011fc88262e7bbd854e6dc2a83a6e
seyoung8239 <seyoung8239@gmail.com>
2022-06-08 02:29:36 +0900

    Docs: fix shell script errors

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/bash
 cd backend
+apt update
+apt-get install python3 python3-pip python3-venv
 
 python -m venv venv
 
@@ -8,12 +11,13 @@ pip install -r requirements.txt
 
 touch .env
 echo "API_KEY=AIzaSyDpfOdjNdYlEiOngbhmRF-MZWlF8rOdnEM" >> .env
-ehco "DB_URL=mongodb+srv://admin:admin@cluster0.soqt1rm.mongodb.net/?retryWrites=true&w=majority" >> .env
+echo "DB_URL=mongodb+srv://admin:admin@cluster0.soqt1rm.mongodb.net/?retryWrites=true&w=majority" >> .env
 
-flask run
-gnome-terminal
+nohup flask run > log.txt 2>&1 &
 
 # start frontend project
-cd frontend
+apt install -y nodejs npm 
+cd ../frontend
+pwd
 npm i
 npm start
